66289de28ee2762ba3834c4fa3a39c9d5ec301ef
april <996586214@qq.com>
2020-04-26 22:38:07 +0800

    update nossr

--- src/templates/tag/tag.js
+++ src/templates/tag/tag.js
@@ -13,6 +13,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Slide from '@material-ui/core/Slide';
 import Box from '@material-ui/core/Box';
+import NoSsr from '@material-ui/core/NoSsr';
 
 import Tags from '@/components/Tags';
 import PostCard from '@/components/Card/post';
@@ -21,7 +22,7 @@ const useStyles = makeStyles(theme => ({
   backgroundiv: {
     height: '50vh',
     overflow: 'hidden',
-    background: `/url(/longmao.png) center`,
+    background: `url(/longmao.png) center`,
     backgroundPositionY: '-25rem',
   },
   paper: {
@@ -46,56 +47,58 @@ const TagPage = ({ data, pageContext }) => {
   return (
     <Layout>
       <div className={classes.backgroundiv} />
-      <Container>
+      <NoSsr>
+        <Container>
 
-        <Paper elevation={3} className={`${classes.paper} ${classes.paperTop} ${classes.tagCtn}`}>
-          <Tags tags={tags} select={tag} type={useSearchParam("t")} />
-        </Paper>
-        <div className="col-xl-10 col-lg-7 col-md-12 col-xs-12 order-2">
-          <div
-            className="col-12"
-            style={{
-              fontSize: 20,
-              margin: 15,
-            }}
-          >
-            {edges.length}
+          <Paper elevation={3} className={`${classes.paper} ${classes.paperTop} ${classes.tagCtn}`}>
+            <Tags tags={tags} select={tag} type={useSearchParam("t")} />
+          </Paper>
+          <div className="col-xl-10 col-lg-7 col-md-12 col-xs-12 order-2">
+            <div
+              className="col-12"
+              style={{
+                fontSize: 20,
+                margin: 15,
+              }}
+            >
+              {edges.length}
               &nbsp;Articles in&nbsp;
               {tag}
+            </div>
+            <Grid container spacing={2}>
+              {
+                edges.map(({ node }) => {
+                  const { frontmatter, fields: { path } } = node;
+                  const image = frontmatter.image || 'http://static.blinkfox.com/20190302.png';
+                  const formatDate = dayjs(frontmatter.date).format('YYYY-MM-DD');
+                  const { title, description, categories } = frontmatter;
+
+                  return (
+                    <Grid item xs={12} sm={6} md={4} key={title}>
+
+                      <Slide direction="left" in={true} mountOnEnter unmountOnExit timeout={800}>
+                        <Box>
+                          <PostCard
+                            path={path}
+                            image={image}
+                            date={formatDate}
+                            title={title}
+                            description={description}
+                            tagShow={false}
+                            categories={categories}
+                          />
+                        </Box>
+                      </Slide>
+                    </Grid>
+                  )
+                })
+              }
+            </Grid>
+
           </div>
-          <Grid container spacing={2}>
-            {
-              edges.map(({ node }) => {
-                const { frontmatter, fields: { path } } = node;
-                const image = frontmatter.image || 'http://static.blinkfox.com/20190302.png';
-                const formatDate = dayjs(frontmatter.date).format('YYYY-MM-DD');
-                const { title, description, categories } = frontmatter;
-
-                return (
-                  <Grid item xs={12} sm={6} md={4} key={title}>
-
-                    <Slide direction="left" in={true} mountOnEnter unmountOnExit timeout={800}>
-                      <Box>
-                        <PostCard
-                          path={path}
-                          image={image}
-                          date={formatDate}
-                          title={title}
-                          description={description}
-                          tagShow={false}
-                          categories={categories}
-                        />
-                      </Box>
-                    </Slide>
-                  </Grid>
-                )
-              })
-            }
-          </Grid>
-
-        </div>
-        <div className="col-xl-2 col-lg-1 order-3" />
-      </Container>
+          <div className="col-xl-2 col-lg-1 order-3" />
+        </Container>
+      </NoSsr>
     </Layout>
   );
 };
